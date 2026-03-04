27c81a7eae9927f89a40f3dd42cb89a98703e3d1
Bagirov24 <79216053+Bagirov24@users.noreply.github.com>
2026-03-04 18:40:40 +0300

    Update models __init__: export all 8 models including budget/goal/recurring/achievement

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,24 @@
-from .user import User
-from .transaction import Transaction
+from .user import User, SubscriptionPlan
+from .transaction import Transaction, TransactionType
 from .investment import Investment
 from .notification import Notification
+from .budget import Budget, BudgetPeriod
+from .goal import Goal
+from .recurring_payment import RecurringPayment, RecurringFrequency
+from .achievement import Achievement, UserXP
+
+__all__ = [
+    "User",
+    "SubscriptionPlan",
+    "Transaction",
+    "TransactionType",
+    "Investment",
+    "Notification",
+    "Budget",
+    "BudgetPeriod",
+    "Goal",
+    "RecurringPayment",
+    "RecurringFrequency",
+    "Achievement",
+    "UserXP",
+]
