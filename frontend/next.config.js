f3886c34e343d746cfcdf3c112186ef481b8f92c
Bagirov24 <79216053+Bagirov24@users.noreply.github.com>
2026-03-04 17:08:02 +0300

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+  i18n: {
+    locales: ['ru', 'en'],
+    defaultLocale: 'ru',
+  },
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
+          { key: 'Service-Worker-Allowed', value: '/' },
+        ],
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
